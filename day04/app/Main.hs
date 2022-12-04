4a74c254ab247156489cc815f8e1f45dc0e74c62
davidhoerster <david@agileways.com>
2022-12-04 08:04:01 -0500

    day4 solutions for a and b

--- day04/app/Main.hs
+++ day04/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.IO  
 import Data.List 
+import Data.List.Split
 
 main :: IO ()
 main = do
@@ -14,8 +15,37 @@ main = do
 
 
 day4a :: String -> Int
-day4a contents = 0
+day4a contents = length $ filter (==True) $ map checkFullyContain $ lines contents
 
 
 day4b :: String -> Int
-day4b contents = 0
+day4b contents = length $ filter (==True) $ map checkOverlap $ lines contents
+
+
+checkFullyContain :: String -> Bool
+checkFullyContain line = 
+    let (a,b,c,d) = getIntParts line
+    in  ((a,b) `containedBy` (c,d)) || ((c,d) `containedBy` (a,b))
+
+
+checkOverlap :: String -> Bool
+checkOverlap line =
+    let (a,b,c,d) = getIntParts line
+    in ((a `between` (c,d)) || (b `between` (c,d))) || ((c `between` (a,b)) || (d `between` (a,b)))
+
+
+getIntParts :: String -> (Integer, Integer, Integer, Integer)
+getIntParts line = 
+    let parts = splitOneOf "-," line
+        intParts = map (\z -> read z :: Integer) parts
+        a = intParts !! 0
+        b = intParts !! 1
+        c = intParts !! 2
+        d = intParts !! 3
+    in (a,b,c,d)
+
+between :: Integer -> (Integer, Integer) -> Bool
+between point (start,end) = point >= start && point <= end
+
+containedBy :: (Integer, Integer) -> (Integer, Integer) -> Bool
+containedBy (innerStart,innerEnd) (outerStart,outerEnd) = (innerStart >= outerStart) && (innerEnd <= outerEnd)
--- day04/dist-newstyle/build/x86_64-linux/ghc-9.2.4/day04-0.1.0.0/x/day04/build/day04/autogen/cabal_macros.h
+++ day04/dist-newstyle/build/x86_64-linux/ghc-9.2.4/day04-0.1.0.0/x/day04/build/day04/autogen/cabal_macros.h
@@ -20,6 +20,16 @@
   (major1) == 4 && (major2) <  16 || \
   (major1) == 4 && (major2) == 16 && (minor) <= 3)
 #endif /* MIN_VERSION_base */
+/* package split-0.2.3.5 */
+#ifndef VERSION_split
+#define VERSION_split "0.2.3.5"
+#endif /* VERSION_split */
+#ifndef MIN_VERSION_split
+#define MIN_VERSION_split(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  2 || \
+  (major1) == 0 && (major2) == 2 && (minor) <= 3)
+#endif /* MIN_VERSION_split */
 
 /* tool gcc-9 */
 #ifndef TOOL_VERSION_gcc
